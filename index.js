7b440244d57243c2f711aee156e2f4bf1f197aa5
Azmal <azmalgazi8@gmail.com>
2023-06-13 19:41:26 +0600

    sort with top payment class

--- index.js
+++ index.js
@@ -184,6 +184,14 @@ async function run() {
       res.send(result);
     });
 
+
+    app.get("/topClass", async (req, res) => {
+      const result = await classesCollection.find().sort({'booking': -1}).toArray();
+      res.send(result);
+    });
+
+    
+
     app.get("/class/:email", async (req, res) => {
       const email = req.params.email;
       // console.log(email);
