cd69cf67e3419a7de04956885cccf982ed632ae1
Azmal <azmalgazi8@gmail.com>
2023-06-14 18:49:53 +0600

    delete user and selected class

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(express.json());
 
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
-  // console.log(authorization)
   if (!authorization) {
     return res
       .status(401)
@@ -58,9 +57,15 @@ async function run() {
 
     const usersCollection = client.db("sportsAcademic").collection("users");
     const classesCollection = client.db("sportsAcademic").collection("classes");
-    const savedCollection = client.db("sportsAcademic").collection("savedClasses");
-    const paymentCollection = client.db("sportsAcademic").collection("payments");
-    const feedbackCollection = client.db("sportsAcademic").collection("feedback");
+    const savedCollection = client
+      .db("sportsAcademic")
+      .collection("savedClasses");
+    const paymentCollection = client
+      .db("sportsAcademic")
+      .collection("payments");
+    const feedbackCollection = client
+      .db("sportsAcademic")
+      .collection("feedback");
 
     app.post("/jwt", (req, res) => {
       const user = req.body;
@@ -87,6 +92,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log(user);
@@ -110,7 +122,6 @@ async function run() {
       const query = { email: email };
       const user = await usersCollection.findOne(query);
       const result = { admin: user?.role === "admin" };
-      // console.log(result);
       res.send(result);
     });
 
@@ -125,7 +136,6 @@ async function run() {
       const user = await usersCollection.findOne(query);
 
       const result = { instructor: user?.role === "instructor" };
-      //  console.log(result);
       res.send(result);
     });
 
@@ -140,13 +150,11 @@ async function run() {
       const user = await usersCollection.findOne(query);
 
       const result = { student: user?.role === "student" };
-      //  console.log(result);
       res.send(result);
     });
 
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
-      // console.log(id);
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
@@ -154,13 +162,11 @@ async function run() {
         },
       };
       const result = await usersCollection.updateOne(filter, updateDoc);
-      // console.log(result)
       res.send(result);
     });
 
     app.patch("/users/instructor/:id", async (req, res) => {
       const id = req.params.id;
-      // console.log(id);
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
@@ -173,7 +179,6 @@ async function run() {
 
     app.get("/instructor", async (req, res) => {
       const query = { role: "instructor" };
-      // console.log(query);
       const result = await usersCollection.find(query).toArray();
       res.send(result);
     });
@@ -185,33 +190,30 @@ async function run() {
       res.send(result);
     });
 
-
     app.get("/topClass", async (req, res) => {
-      const result = await classesCollection.find().sort({'booking': -1}).toArray();
+      const result = await classesCollection
+        .find()
+        .sort({ booking: -1 })
+        .toArray();
       res.send(result);
     });
 
-    
-
     app.get("/class/:email", async (req, res) => {
       const email = req.params.email;
-      // console.log(email);
+     
       const query = { instructorEmail: email };
-      // console.log(query);
       const result = await classesCollection.find(query).toArray();
       res.send(result);
     });
 
     app.get("/approve", async (req, res) => {
       const query = { status: "approve" };
-      // console.log(query);
       const result = await classesCollection.find(query).toArray();
       res.send(result);
     });
 
     app.post("/class", async (req, res) => {
       const newItem = req.body;
-      // console.log(newItem);
       const result = await classesCollection.insertOne(newItem);
       res.send(result);
     });
@@ -226,9 +228,7 @@ async function run() {
     app.patch("/class/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
-      // const options = {upsert: true};
       const updateDoc = {
-        //  seats: { $lt: 0 } ,
         $inc: {
           seats: -1,
         },
@@ -245,15 +245,12 @@ async function run() {
           status: "approve",
         },
       };
-      // console.log(updateDoc);
       const result = await classesCollection.updateOne(filter, updateDoc);
-      // console.log(result);
       res.send(result);
     });
 
     app.patch("/class/deny/:id", async (req, res) => {
       const id = req.params.id;
-      // console.log(id);
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
@@ -266,12 +263,10 @@ async function run() {
 
     //saved calls post
 
-
     app.get("/savedClass", async (req, res) => {
-      const {id} = req.query
-      // console.log(id)
-      const query = {_id: new ObjectId(id)}
-      const result = await savedCollection.findOne(query)
+      const { id } = req.query;
+      const query = { _id: new ObjectId(id) };
+      const result = await savedCollection.findOne(query);
       res.send(result);
     });
 
@@ -282,51 +277,43 @@ async function run() {
 
     app.get("/savedClass/:email", async (req, res) => {
       const email = req.params.email;
-      // console.log(email);
+     
       const query = { studentEmail: email };
-      // console.log(query);
       const result = await savedCollection.find(query).toArray();
       res.send(result);
     });
 
-    
-   
-
-    
-
-    app.post("/savedClass", async (req, res) => {
-    
-      const saved = req.body;      
-      // console.log(saved);
-      
-      const email =saved.studentEmail
-        const name =  saved.name;
-      const query = {
-        $and: [
-          { name: { $eq: name } },
-          { studentEmail: { $eq: email } }
-        ]
-      };
-      
-      // console.log(query);
-      const existingClass = await savedCollection.findOne(query);
-      // console.log("Existing Class", existingClass);
-
-      if (existingClass) {
-          return res.send({ message: "Class  already exists" });
-        }
-      const result = await savedCollection.insertOne(saved);
-      // console.log(result);
+    app.delete("/savedClass/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await savedCollection.deleteOne(query);
       res.send(result);
     });
 
 
 
-     // create payment intent
-     app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+    app.post("/savedClass", async (req, res) => {
+      const saved = req.body;
+
+      const email = saved.studentEmail;
+      const name = saved.name;
+      const query = {
+        $and: [{ name: { $eq: name } }, { studentEmail: { $eq: email } }],
+      };
+
+      const existingClass = await savedCollection.findOne(query);
+
+      if (existingClass) {
+        return res.send({ message: "Class  already exists" });
+      }
+      const result = await savedCollection.insertOne(saved);
+      res.send(result);
+    });
+
+    // create payment intent
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
       const { price } = req.body;
       const amount = parseInt(price * 100);
-      // console.log(amount)
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: "usd",
@@ -338,134 +325,70 @@ async function run() {
       });
     });
 
-     //payment related API
+    //payment related API
 
-     app.post("/payments/:id", verifyJWT, async (req, res) => {
+    app.post("/payments/:id", verifyJWT, async (req, res) => {
       const payment = req.body;
       payment.createAt = new Date();
-      // console.log(payment)
-     
+
       const insertResult = await paymentCollection.insertOne(payment);
-        const id =req.params.id
+      const id = req.params.id;
       const query = {
-        _id:new ObjectId(id),
+        _id: new ObjectId(id),
       };
       const deleteResult = await savedCollection.deleteMany(query);
 
-      // const filter={name:payment.name}
-      // const findResult =await classesCollection.findOne(filter)
-      // console.log(findResult)
-      // const updateDoc ={
-      //   $inc: {booking : 1}
-      // }
-      // const updateResult = await classesCollection.updateOne(findResult, updateDoc)
-      //    console.log(updateResult)
+     
       res.send({ insertResult, deleteResult });
     });
 
-
     app.put("/payment/:name", async (req, res) => {
       const name = req.params.name;
-      // console.log(name);
-      const filter = { name:name };
-      // const options = {upsert: true};
-      const options ={upsert: true}
+      const filter = { name: name };
+      const options = { upsert: true };
       const updateDoc = {
-        //  seats: { $lt: 0 } ,
         $inc: {
           booking: 1,
         },
       };
-      const result = await classesCollection.updateOne(filter, updateDoc, options);
-      console.log(result)
+      const result = await classesCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      console.log(result);
       res.send(result);
     });
 
-
     app.get("/payment/:email", async (req, res) => {
       const email = req.params.email;
-      // console.log(email);
+     
       const query = { studentEmail: email };
       // console.log(query);
       const result = await paymentCollection.find(query).toArray();
       res.send(result);
     });
 
-//sort with descending
+    //sort with descending
     app.get("/history/:email", async (req, res) => {
       const email = req.params.email;
-      // console.log(email);
+     
       // const createAt = new Date()
       const query = { studentEmail: email };
       // console.log(query);
-      const result = await paymentCollection.find(query).sort({createdAt: -1}).toArray();
+      const result = await paymentCollection
+        .find(query)
+        .sort({ createdAt: -1 })
+        .toArray();
       res.send(result);
     });
 
-
-
-    app.get('/payment', async (req, res)=>{
+    app.get("/payment", async (req, res) => {
       const result = await paymentCollection.find().toArray();
       res.send(result);
-    })
+    });
 
-
-    
-
-    // app.get("/admin-stats", verifyJWT, verifyAdmin, async (req, res) => {
-    //   const users = await usersCollection.estimatedDocumentCount();
-    //   const products = await menuCollection.estimatedDocumentCount();
-    //   const orders = await paymentCollection.estimatedDocumentCount();
-    //   const payments = await paymentCollection.find().toArray();
-    //   const revenue = payments.reduce((sum, payment) => sum + payment.price, 0);
-    //   //best way to sum of a field is to use group and sum operato
-    //   res.send({
-    //     users,
-    //     products,
-    //     orders,
-    //     revenue,
-    //   });
-    // });
-
-    // app.get("/order-stats", verifyJWT,verifyAdmin, async (req, res) => {
-    //   const pipeline = [
-    //     {
-    //       $lookup: {
-    //         from: 'menu',
-    //         localField: 'menuItems',
-    //         foreignField: '_id',
-    //         as: 'menuItemsData'
-    //       }
-    //     },
-    //     {
-    //       $unwind: '$menuItemsData'
-    //     },
-    //     {
-    //       $group: {
-    //         _id: '$menuItemsData.category',
-    //         count: { $sum: 1 },
-    //         total: { $sum: '$menuItemsData.price' }
-    //       }
-    //     },
-    //     {
-    //       $project: {
-    //         category: '$_id',
-    //         count: 1,
-    //         total: { $round: ['$total', 2] },
-    //         _id: 0
-    //       }
-    //     }
-    //   ];
-
-    //   const result = await paymentCollection.aggregate(pipeline).toArray()
-    //   res.send(result)
-    // });
-
-
-
-
-
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
